17557fcd9f801874b937ba793437410a08539dfb
Sim30n <nomail>
2021-06-07 14:01:54 +0300

    Model for race and basic project setup

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# karting-v2
+
+
+# Make virtual environment 
+
+## On windows
+Virtualenv
+py -m venv env
+.\venv\Scripts\activate
+
+## On Linux
+virtualenv venv
+venv\bin\activate
+
+## Install requirements
+pip install -r requirements.txt
+
+
+----------------------------
+# Flask config to local environment
+
+## On Windows 
+$env:FLASK_APP ="models.py"
+$env:FLASK_ENV="development"
+$env:DEBUG=1
+
+## On Linux
+export FLASK_APP ="models.py"
+export FLASK_ENV="development"
+export DEBUG=1
---
+++ app.py
@@ -0,0 +1,48 @@
+import os
+from flask import Flask, render_template, url_for, redirect
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+load_dotenv()
+
+######################################
+#### SET UP OUR SQLite DATABASE #####
+####################################
+
+# This grabs our directory
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+# Connects our Flask App to our Database
+DATABASE_URL = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Add on migration capabilities in order to run terminal commands
+Migrate(app,db)
+
+class Race(db.Model):
+
+    __tablename__ = 'races'
+
+    id = db.Column(db.Integer,primary_key=True)
+    location = db.Column(db.Text)
+    race_date = db.Column(db.DateTime)
+
+    def __init__(self,location,race_date):
+        self.location = location
+        self.race_date = race_date
+
+    def __repr__(self):
+
+        return f"Race {self.location} : {self.race_date}"
+
+@app.route('/')
+def index():
+    races = Race.query.all()
+    return render_template("home.html", races=races)
+
+
