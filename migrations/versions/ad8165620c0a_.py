792b56a46484e7e530ec7b717e98978d9ee00beb
Sim30n <nomail>
2021-06-15 16:42:50 +0300

    migrations

---
+++ migrations/versions/ad8165620c0a_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: ad8165620c0a
+Revises: 
+Create Date: 2021-06-15 16:41:32.973274
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ad8165620c0a'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
