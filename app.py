b21720e1254a17c2457bf137fe19b600f212cca0
Petteri Särkkä <petteri.sarkka@gmail.com>
2021-06-20 23:43:07 +0300

    fix

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ def index():
         queries.append(Driver.name.ilike(driver_name))
     if location:
         queries.append(Race.location.ilike(search_location))
-    print(queries)
+    #print(queries)
     
 
     #results = Result.query.all()
@@ -125,7 +125,7 @@ def index():
 
     """ Get lap times by location and driver name """
     lap_times = Lap.query.join(Race).join(Driver).filter(*queries).all()
-    print(dir(lap_times[0]))
+    #print(dir(lap_times[0]))
     #print(result[0].driver.name)
     #print(dir(test_query[0]))
     #print(test_query[0].driver.name)
